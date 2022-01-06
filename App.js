e0d6662efa22089959398203b25994d292bb61f3
gints_perkons <gintskristaps13@gmail.com>
2022-01-06 15:42:45 +0200

    new

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style= {styles.text} style={{fontSize:36}} >Hello World!{"\n"}{"\n"}</Text>
+      <Image source={{uri: 'https://miro.medium.com/max/3200/1*0KFB17_NGTPB0XWyc4BSgQ.jpeg'}} style={styles.img}/>
+      <Text style={styles.text}>Hello World from group 13 Gints Kristaps Pērkons!{"\n"}{"\n"}</Text>
+      <Text style={styles.text}>This is my Gints Kristaps Pērkons first React Native application!</Text>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -14,8 +17,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#19f5a9' ,
   },
+  img: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width:  200,
+    height: 100,
+    borderRadius: 1000,
+  },
+  text: {
+    fontWeight: 'bold' ,
+  }
 });
