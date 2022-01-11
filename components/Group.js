1079d6911bc570658dd5b670d0c2472627b23db7
Sandis <sandiss2013@gmail.com>
2022-01-11 18:25:02 +0200

    ttt
    tttttt

--- components/Group.js
+++ components/Group.js
@@ -13,7 +13,7 @@ const Group = (props) => {
 
 const styles = StyleSheet.create({
     group:{
-        backgroundColor:'#f0f',
+        backgroundColor:'#fff',
         borderRadius:10,
         padding:10,
         flexDirection: 'row',
